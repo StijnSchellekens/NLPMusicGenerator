0a7b62d14c8a7e8e9243115bc97f0a959de803bd
Stijn Schellekens <459890@student.fontys.nl>
2023-12-03 19:32:44 +0100

    Created readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # NLPMusicGenerator
+### Design a streamlit application to enable a user to use language to generate a melody with a specific description.
+
+For this application I used the [Magenta](https://magenta.tensorflow.org/) library to generate music. I used the [Music_Vae](https://magenta.tensorflow.org/music-vae) model to generate music.
+For the NLP part of the exercise I used BERT to generate embeddings for the text input and then used cosine similarity to find the closest match to the input text from a set of pre-defined descriptions.
+
+
+![Diagram.png](Diagram.png)
+
+### How does it work
+Describe the general mood of the melody you want to generate. This can be anything from a calm melody to an energetic one. You can also describe things like environments where a melody would be used in such as a library or a festival (with varying results).
+
+### Demo
+[Demo video](https://www.youtube.com/watch?v=EwsKOAwQ3NU)
--- src/bert.py
+++ src/bert.py
@@ -5,15 +5,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
 
+# A method that gets the BERT embedding for a given text. This will be used for both the pre-set embeddings and the user input.
 def get_bert_embedding(text):
     inputs = tokenizer(text, return_tensors='pt')
     outputs = model(**inputs)
     return outputs.last_hidden_state.mean(dim=1).detach().numpy()
 
+# A method that gets the closest pre-set feature to a given user embedding.
 def get_closest_feature(embeddings, user_embedding):
     user_embedding_2d = user_embedding.reshape(1, -1)
 
-    # Debugging: Print out similarity scores
+    # Debugging: Print out similarity scores for each feature
     for feature, embedding in embeddings.items():
         score = cosine_similarity(embedding.reshape(1, -1), user_embedding_2d)[0][0]
         print(f"Similarity with {feature}: {score}")
